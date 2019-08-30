d0d598561f979c843cc3d280462af9d61808e7e7
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-08-30 11:57:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,6 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-my_array = ["wow","I'm", "learning", "arrays!"]
-  my_array.shift
+my_array = ["wow","I'm","really", "learning", "arrays!"]
+  my_array
 end
