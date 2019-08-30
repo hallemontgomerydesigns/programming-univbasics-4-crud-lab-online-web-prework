39fc61361e4357bd44bdc4c5eee538f70fbae1c7
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-08-30 11:49:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = [1,2,3,4]
-  my_array.unshift (0)
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+ 
+famous_cats.unshift("nala cat")
 end
 
 def remove_element_from_end_of_array(array)
